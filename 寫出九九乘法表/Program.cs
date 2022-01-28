3a088cd8e5a1df2e4f4238aa74e63f2df06b9ae9
翎漪 何 <a0973568600@gmail.com>
2022-01-28 18:27:37 +0800

    加入專案檔案。

---
+++ 寫出九九乘法表/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("九九乘法表");
+            string text = "";
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    text = i + "x" + j + "=" + i * j;
+                    Console.Write(text + "\t");
+                }
+                Console.Write("\r\n");
+            }
+
+        }
+    }
+}
+
+
